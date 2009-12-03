a168668123eba8528405f22f0f2de9f9f3d037af
Julien 'Lta' BALLET <elthariel@gmail.com>
2009-12-03 18:53:16 +0100

    AudioFile: remove new, create actions. Updated configuration with incomingd parameter. Added incomingd, the 'daemon' responsible for converting audio files that arrives in IncomingAudioFiles queue/table

--- app/controllers/audio_files_controller.rb
+++ app/controllers/audio_files_controller.rb
@@ -2,43 +2,35 @@ class AudioFilesController < ApplicationController
   def index
     @audio_files = AudioFile.all
   end
-  
+
   def show
     @audio_file = AudioFile.find(params[:id])
   end
-  
-  def new
-    @audio_file = AudioFile.new
-  end
-  
-  def create
-    @audio_file = AudioFile.new(params[:audio_file])
-    if @audio_file.save
-      flash[:notice] = "Successfully created audio file."
-      redirect_to @audio_file
-    else
-      render :action => 'new'
-    end
-  end
-  
+
   def edit
     @audio_file = AudioFile.find(params[:id])
   end
-  
+
   def update
     @audio_file = AudioFile.find(params[:id])
     if @audio_file.update_attributes(params[:audio_file])
-      flash[:notice] = "Successfully updated audio file."
-      redirect_to @audio_file
+      flash[:notice] = "Fichier mis a jour"
+      redirect_to audio_files_url
     else
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @audio_file = AudioFile.find(params[:id])
-    @audio_file.destroy
-    flash[:notice] = "Successfully destroyed audio file."
+    if File.writable?(@audio_file.path)
+      #FIXME security
+      `rm "#{@audio_file.path}"`
+      @audio_file.destroy
+      flash[:notice] = "Fichier supprime"
+    else
+      flash[:error] = "Impossible de supprimer le fichier"
+    end
     redirect_to audio_files_url
   end
 end
