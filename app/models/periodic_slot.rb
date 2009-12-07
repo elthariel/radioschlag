a6825166d7bdfbed3512a247d34fa422448ca18a
Julien 'Lta' BALLET <elthariel@gmail.com>
2009-12-07 03:06:10 +0100

    Revert "Added Slot models"
    This reverts commit 88e71cbe455ba525b6ad5fabf2bf01d7381a46df.

--- app/models/periodic_slot.rb
+++
@@ -1,3 +0,0 @@
-class PeriodicSlot < ActiveRecord::Base
-  attr_accessible :name, :desc, :start, :end, :program_id
-end
--- app/models/slot.rb
+++
@@ -1,3 +0,0 @@
-class Slot < ActiveRecord::Base
-  attr_accessible :name, :desc, :start, :end, :program_id
-end
