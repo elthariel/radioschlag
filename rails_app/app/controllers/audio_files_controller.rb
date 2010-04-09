51a6b26cf37d142ca9d81db37ed98c7a282ca403
Julien 'Lta' BALLET <elthariel@gmail.com>
2010-04-09 03:06:21 +0200

    Added get parameters for selecting which files to display

--- rails_app/app/controllers/audio_files_controller.rb
+++ rails_app/app/controllers/audio_files_controller.rb
@@ -3,10 +3,12 @@ class AudioFilesController < ApplicationController
 
   def index
 
+    cond = index_conditions(params)
+
     respond_to do |format|
       format.html do
         # @posts = Post.paginate_by_board_id @board.id, :page => params[:page], :order => 'updated_at DESC'
-        @audio_files = AudioFile.paginate :page => params[:page], :order => 'user_id'
+        @audio_files = AudioFile.paginate :page => params[:page], :order => 'user_id', :conditions => cond
         render :action => 'index'
       end
       format.json do
@@ -42,4 +44,28 @@ class AudioFilesController < ApplicationController
     end
     redirect_to audio_files_url
   end
+
+  protected
+  def index_conditions(params)
+    cond = {}
+
+    # Owner condition
+    if params[:user]
+      puts "\n\n\n\n\n\n\n#{params[:user]} #{current_user.id}\n\n\n\n\n\n\n\n"
+      if params[:user] == 'me' and current_user
+        cond[:user_id] = current_user.id
+      else
+        id = User.first(:conditions => {:username => params[:user]}).id
+        cond[:user_id] = id if id
+      end
+    end
+
+    # Type and Style conditions
+    cond[:audio_file_type_id] = params[:type] if params[:type]
+    cond[:audio_file_style_id] = params[:style] if params[:style]
+
+    # Return the built conditions
+    cond
+  end
+
 end
